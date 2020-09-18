7f42987ad7e1bca0285e0f93e7272e31e22801fe
mlcivilengineer <65793493+mlcivilengineer@users.noreply.github.com>
2020-09-18 13:40:05 -0300

    Update format_shell.sh

--- format_shell.sh
+++ format_shell.sh
@@ -165,3 +165,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 sudo apt-get install autojump
 sudo apt-get install fzf
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
+
+# Install kubectl
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
