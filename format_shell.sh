55f87f644bf06e81b8e9470908dc9982efb379c2
mlcivilengineer <65793493+mlcivilengineer@users.noreply.github.com>
2020-09-18 13:26:05 -0300

    Update format_shell.sh

--- format_shell.sh
+++ format_shell.sh
@@ -160,3 +160,8 @@ sudo apt install zsh
 
 # Installing oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Installing zsh plugins:
+sudo apt-get install autojump
+sudo apt-get install fzf
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
