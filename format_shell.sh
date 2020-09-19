a0aa7995036e3f27b7bf5a393574381bca7efba6
mlcivilengineer <65793493+mlcivilengineer@users.noreply.github.com>
2020-09-19 14:24:50 -0300

    Update format_shell.sh

--- format_shell.sh
+++ format_shell.sh
@@ -180,3 +180,9 @@ sudo mv k9s /usr/local/bin
 # Install Dive tool for docker
 wget https://github.com/wagoodman/dive/releases/download/v0.9.2/dive_0.9.2_linux_amd64.deb
 sudo apt install ./dive_0.9.2_linux_amd64.deb
+
+# Install github CLI
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
