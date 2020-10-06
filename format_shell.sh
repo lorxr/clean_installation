aacdc881e46f9ab5746b8b33c6646d37e27ba2d5
mlcivilengineer <65793493+mlcivilengineer@users.noreply.github.com>
2020-10-06 00:24:47 -0300

    Update format_shell.sh

--- format_shell.sh
+++ format_shell.sh
@@ -196,3 +196,10 @@ sudo apt-get install tesseract-ocr-por
 
 # Install imagemagick
 sudo apt install imagemagick
+
+# Install helm
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
