769d7d6b95d3f153a423c6ed1f3e534436d3329a
mlcivilengineer <65793493+mlcivilengineer@users.noreply.github.com>
2020-10-06 00:32:20 -0300

    Update format_shell.sh

--- format_shell.sh
+++ format_shell.sh
@@ -203,3 +203,7 @@ sudo apt-get install apt-transport-https --yes
 echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
 sudo apt-get install helm
+
+# Install kustomize
+curl -s "https://raw.githubusercontent.com/\
+kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
