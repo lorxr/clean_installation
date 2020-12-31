fd702ef2dd8b0e51814655cc42cc59a8c2cf7605
mlcivilengineer <65793493+mlcivilengineer@users.noreply.github.com>
2020-12-31 19:29:21 -0300

    adds terraform

--- format_shell.sh
+++ format_shell.sh
@@ -215,3 +215,8 @@ mv ./argocd-linux-amd64 /usr/bin/argocd
 
 # Install traceroute
 sudo apt-get install traceroute
+
+# Install Terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt install terraform
