5fb85706134f28f9303517113cc5ac698dde1b71
mlcivilengineer <65793493+mlcivilengineer@users.noreply.github.com>
2021-01-02 02:43:25 -0300

    Update format_shell.sh

--- format_shell.sh
+++ format_shell.sh
@@ -227,5 +227,8 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt install terraform
 
+# Install flameshot clipping tool
+sudo apt install flameshot
+
 
 sudo reboot
