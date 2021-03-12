e6e1d71992e0a622cb2e5dabf44f8e2261ba9b42
Lua <mlcivilengineer@users.noreply.github.com>
2021-03-11 23:25:34 -0300

    adiciona -y para tudo

--- format_shell.sh
+++ format_shell.sh
@@ -7,21 +7,17 @@ sudo apt-get update
 timedatectl set-local-rtc 1 --adjust-system-clock
 
 # Install zsh
-sudo apt install zsh
+sudo apt -y install zsh
 
 # Installing oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Installing zsh plugins:
-sudo apt-get install autojump
-sudo apt-get install fzf
+sudo apt-get -y install autojump
+sudo apt-get -y install fzf
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 cp ./.zshrc $HOME/.zshrc
-
-# Making zsh the default shell (applied after a user logout)
-chsh -s $(which zsh)
-
 
 # Install Pycharm
 sudo snap install pycharm-community --classic
@@ -30,17 +26,17 @@ sudo snap install pycharm-community --classic
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt-get -y install google-chrome-stable
 
 # Install git
-sudo apt-get install git
+sudo apt-get -y install git
 git config credential.helper cache
 
 # Install pip for python3
-sudo apt install python3-pip
+sudo apt -y install python3-pip
 
 # Install Docker
-sudo apt-get install \
+sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -52,7 +48,7 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 #sudo snap install docker
 
@@ -66,10 +62,10 @@ sudo snap install plexmediaserver
 #sudo bash -c "echo '/dev/sda1 /mnt/media ntfs defaults 0 0' >> /etc/fstab"
 
 # Install qBittorrent
-sudo apt-get install qbittorrent
+sudo apt-get -y install qbittorrent
 
 # Install Nemo
-sudo apt install nemo
+sudo apt -y install nemo
 
 # Making Nemo the default file manager
 xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
@@ -79,26 +75,26 @@ xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
 sudo snap install vlc
 
 # Install Chrome Gnome Shell
-sudo apt-get install chrome-gnome-shell
+sudo apt-get -y install chrome-gnome-shell
 
 # Install Gnome Tweak Tool
-sudo apt-get install gnome-tweak-tool
+sudo apt-get -y install gnome-tweak-tool
 
 # Install prefs to fix tweaks bug
-sudo apt install gnome-shell-extension-prefs
+sudo apt -y install gnome-shell-extension-prefs
 
-sudo apt-get install gnome-shell-extension-dash-to-panel
+sudo apt-get -y install gnome-shell-extension-dash-to-panel
 
 # Install Spotify
 sudo snap install spotify
 
 # Install Flatpak
-sudo apt install flatpak
-sudo apt install gnome-software-plugin-flatpak
+sudo apt -y install flatpak
+sudo apt -y install gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install Gdebi
-sudo apt install gdebi-core
+sudo apt -y install gdebi-core
 
 # Install Dbeaver
 sudo snap install dbeaver-ce
@@ -122,7 +118,7 @@ sudo snap install slack --classic
 sudo snap install htop
 
 # Install Vim
-sudo apt install vim
+sudo apt -y install vim
 
 # Install pyenv
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
@@ -137,7 +133,7 @@ sudo ln -s ~/.pyenv/bin/pyenv /usr/local/bin/pyenv # creates symlink
 source ~/.zshrc
 
 # Install nmap (for network vulnerabilities)
-sudo apt-get install nmap
+sudo apt-get -y install nmap
 
 # Install NvidiaDrivers
 sudo add-apt-repository ppa:graphics-drivers/ppa
@@ -152,11 +148,11 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
 sudo apt-get update
 wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt -y install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
 sudo apt-get update
 
 # Install development and runtime libraries (~4GB)
-sudo apt-get install --no-install-recommends \
+sudo apt-get -y install --no-install-recommends \
     cuda-10-1 \
     libcudnn7=7.6.4.38-1+cuda10.1  \
     libcudnn7-dev=7.6.4.38-1+cuda10.1
@@ -183,24 +179,24 @@ sudo mv k9s /usr/local/bin
 
 # Install Dive tool for docker
 wget https://github.com/wagoodman/dive/releases/download/v0.9.2/dive_0.9.2_linux_amd64.deb
-sudo apt install ./dive_0.9.2_linux_amd64.deb
+sudo apt -y install ./dive_0.9.2_linux_amd64.deb
 rm ./dive_0.9.2_linux_amd64.deb
 
 # Install github CLI
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 sudo apt-add-repository https://cli.github.com/packages
 sudo apt update
-sudo apt install gh
+sudo apt -y install gh
 
 # Install xclip for copy and pasting
-sudo apt-get install xclip
+sudo apt-get -y install xclip
 
 # Install tesseract for ocr
-sudo apt-get install tesseract-ocr
-sudo apt-get install tesseract-ocr-por
+sudo apt-get -y install tesseract-ocr
+sudo apt-get -y install tesseract-ocr-por
 
 # Install imagemagick
-sudo apt install imagemagick
+sudo apt -y install imagemagick
 
 # Install pic_to_text
 sudo cp -f ./pic_to_text /usr/local/bin/
@@ -211,7 +207,7 @@ curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
 sudo apt-get install apt-transport-https --yes
 echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
-sudo apt-get install helm
+sudo apt-get -y install helm
 
 # Install kustomize
 curl -s "https://raw.githubusercontent.com/\
@@ -224,18 +220,18 @@ sudo chmod +x ./argocd-linux-amd64
 sudo mv ./argocd-linux-amd64 /usr/local/bin/argocd
 
 # Install traceroute
-sudo apt-get install traceroute
+sudo apt-get -y install traceroute
 
 # Install Terraform
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt install terraform
+sudo apt -y install terraform
 
 # Install flameshot clipping tool
-sudo apt install flameshot
+sudo apt -y install flameshot
 
 # Install FileZilla
-sudo apt-get install filezilla
+sudo apt-get -y install filezilla
 
 # Install DrawIO
 sudo snap install drawio
@@ -243,14 +239,14 @@ sudo snap install drawio
 # Install Wireshark
 #sudo add-apt-repository ppa:wireshark-dev/stable#
 #sudo apt-get update
-#sudo apt-get install wireshark
+#sudo apt-get -y install wireshark
 
 # Install VirtualBox
-#sudo apt-get install virtualbox
-#sudo apt-get install virtualbox—ext–pack
+#sudo apt-get -y install virtualbox
+#sudo apt-get -y install virtualbox—ext–pack
 
 # Install Steam
-sudo apt install steam
+sudo apt -y install steam
 
 # Install discord
 wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
@@ -264,7 +260,7 @@ wget -O wallpaper.jpg --header="User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWe
 gsettings set org.gnome.desktop.background picture-uri file:///$PWD/wallpaper.jpg
 
 # Install Dconf
-sudo apt install dconf-cli dconf-editor 
+sudo apt -y install dconf-cli dconf-editor 
 
 # change the dash to panel settings
 dconf load /org/gnome/shell/extensions/dash-to-panel/ < ./dtp_settings
@@ -291,5 +287,8 @@ sudo poetry completions zsh > poetry/_poetry
 flatpak install -y  flathub md.obsidian.Obsidian
 
 sudo apt -y autoremove
+
+# Making zsh the default shell (applied after a user logout)
+chsh -s $(which zsh)
 
 sudo reboot
